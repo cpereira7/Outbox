90d2510846535a4f1bfe58d63b81160bade9d0de
cpereira7 <cpereira7@users.noreply.github.com>
2025-08-23 14:58:02 +0100

    feat(Outbox): Adds the first draft version of the API

---
+++ SampleStack.Outbox/Outbox/Api/Controllers/PackageController.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Outbox.Api.DTOs;
+using Outbox.Infrastructure.Persistence;
+using Outbox.Infrastructure.Service;
+using Outbox.Model;
+
+namespace Outbox.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class PackageController : ControllerBase
+{
+    private readonly PackageDbContext _dbContext;
+    private readonly PackageEventService _packageEventService;
+
+    public PackageController(PackageDbContext dbContext, PackageEventService packageEventService)
+    {
+        _dbContext = dbContext;
+        _packageEventService = packageEventService;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreatePackage([FromBody] CreatePackageRequest request)
+    {
+        await using var transaction = await _dbContext.Database.BeginTransactionAsync();
+        try
+        {
+            var package = new Package(
+                GenerateTrackingCode(),
+                request.ParcelShopId,
+                request.SenderId,
+                request.OriginAddressId,
+                request.DestinationAddressId,
+                request.WeightKg
+            );
+
+            _dbContext.Packages.Add(package);
+            await _dbContext.SaveChangesAsync();
+        
+            var eventAdded = await _packageEventService.CreatePackageEventAsync(
+                package, 
+                "New Package created.", 
+                PackageStatus.Created);
+            
+            if (!eventAdded)
+            {
+                await transaction.RollbackAsync();
+                return Problem("Failed to create package event.", statusCode: 500);
+            }
+            
+            await transaction.CommitAsync();
+
+            var response = new CreatePackageResponse(package.TrackingCode, package.CreatedAt);
+
+            return CreatedAtAction(nameof(GetPackage), new { trackingCode = package.TrackingCode }, response);
+        }
+        catch (Exception ex)
+        {
+            await transaction.RollbackAsync();
+            return Problem(detail: ex.Message, statusCode: 500);
+        }
+    }
+
+    [HttpPost("{trackingCode}")]
+    public async Task<IActionResult> UpdatePackage(string trackingCode, [FromBody] UpdatePackageRequest request)
+    {
+        try
+        {
+            var package = await _dbContext.Packages.FirstOrDefaultAsync(p => p.TrackingCode == trackingCode);
+
+            if (package == null)
+            {
+                return NotFound("Package not found in the system.");
+            }
+            
+            var eventAdded = await _packageEventService.CreatePackageEventAsync(trackingCode, request.Message, request.Status, request.CurrentHubId);
+
+            if (!eventAdded)
+            {
+                return Problem("Failed to add package event.", statusCode: 500);
+            }
+
+            return NoContent();
+        }
+        catch (Exception ex)
+        {
+            return Problem(detail: ex.Message, statusCode: 500);
+        }
+    }
+
+    [HttpGet("{trackingCode}")]
+    public IActionResult GetPackage(string trackingCode)
+    {
+        var package = _dbContext.Packages.FirstOrDefault(p => p.TrackingCode == trackingCode);
+
+        if (package == null)
+        {
+            return NotFound();
+        }
+        return Ok(package);
+    }
+
+    private string GenerateTrackingCode() => $"CTT-9Z-{Random.Shared.Next(1000000000, 1999999999)}";
+}
--- SampleStack.Outbox/Outbox/Program.cs
+++ SampleStack.Outbox/Outbox/Program.cs
@@ -1,8 +1,56 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Outbox.Infrastructure.PackageQueue;
+using Outbox.Infrastructure.Persistence;
+using Outbox.Infrastructure.Service;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers()
+    .ConfigureApiBehaviorOptions(options =>
+    {
+        // To preserve the default behavior, capture the original delegate to call later.
+        var builtInFactory = options.InvalidModelStateResponseFactory;
+
+        options.InvalidModelStateResponseFactory = context =>
+        {
+            var logger = context.HttpContext.RequestServices
+                                .GetRequiredService<ILogger<Program>>();
+
+            logger.LogWarning("Model state invalid: {Errors}", context.ModelState);
+
+            // Invoke the default behavior, which produces a ValidationProblemDetails
+            // response.
+            // To produce a custom response, return a different implementation of 
+            // IActionResult instead.
+            return builtInFactory(context);
+        };
+    });
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<PackageDbContext>(options =>
+    options.UseSqlite("Data Source=testDb.sqlite"));
+
+builder.Services.AddScoped<PackageEventService>();
+builder.Services.AddScoped<IPackageEventQueue, PackageEventQueue>();
+
+builder.Services.AddControllers()
+    .ConfigureApiBehaviorOptions(options =>
+    {
+        options.SuppressConsumesConstraintForFormFileParameters = true;
+        options.SuppressInferBindingSourcesForParameters = true;
+        options.SuppressModelStateInvalidFilter = true;
+        options.SuppressMapClientErrors = true;
+        options.ClientErrorMapping[StatusCodes.Status404NotFound].Link =
+            "https://httpstatuses.com/404";
+    });
+
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
@@ -10,7 +58,11 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.MapControllers();
 
 app.UseHttpsRedirection();
 
